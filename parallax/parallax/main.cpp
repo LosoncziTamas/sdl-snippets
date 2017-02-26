60b2373c3991be951f48fe087de18bf7643bd1af
Tamas Losonczi <losonczitamas@Losonczis-MacBook-Air.local>
2017-02-26 12:34:29 +0100

    clean up parallax sample

--- parallax/parallax/main.cpp
+++ parallax/parallax/main.cpp
@@ -1,37 +1,77 @@
-//
-//  main.cpp
-//  parallax
-//
-//  Created by Losonczi Tamás on 2/5/17.
-//  Copyright © 2017 Losonczi Tamás. All rights reserved.
-//
-
 #include <SDL2/SDL.h>
 #include <SDL2_image/SDL_image.h>
 #include <iostream>
 
+#define SCREEN_WIDTH 640
+#define SCREEN_HEIGHT 480
+
 static bool running;
 
-SDL_Surface *Parallax_LoadImage(char *path, SDL_Surface *surface) {
-    SDL_Surface *optimizedSurface = NULL;
-    SDL_Surface *loadedSurface = IMG_Load(path);
+typedef struct Parallax {
+    SDL_Rect src_rect_1;
+    SDL_Rect src_rect_2;
+    SDL_Rect dst_rect_1;
+    SDL_Rect dst_rect_2;
+    int width;
+    int height;
+} Paralllax;
+
+void init_parallax(Parallax *parallax, int width, int height)
+{
+    parallax->width = width;
+    parallax->height = height;
     
-    if (loadedSurface == NULL)
+    SDL_Rect dst_rect_1 = {};
+    dst_rect_1.h = height;
+    parallax->dst_rect_1 = dst_rect_1;
+    
+    SDL_Rect dst_rect_2 = {};
+    dst_rect_2.w = width;
+    dst_rect_2.h = height;
+    parallax->dst_rect_2 = dst_rect_2;
+    
+    SDL_Rect src_rect_1 = {};
+    src_rect_1.x = width;
+    src_rect_1.h = height;
+    parallax->src_rect_1 = src_rect_1;
+    
+    SDL_Rect src_rect_2 = {};
+    src_rect_2.w = width;
+    src_rect_2.h = height;
+    parallax->src_rect_2 = src_rect_2;
+}
+
+void clip_horizontal_parallax(int start_pos, Parallax *parallax)
+{
+    parallax->dst_rect_1.w = start_pos;
+    parallax->dst_rect_2.x = start_pos;
+    parallax->dst_rect_2.w = parallax->width - start_pos;
+    parallax->src_rect_1.x = parallax->width - start_pos;
+    parallax->src_rect_1.w = start_pos;
+    parallax->src_rect_2.w = parallax->width - start_pos;
+}
+
+SDL_Surface *load_image(char *path, SDL_Surface *surface)
+{
+    SDL_Surface *optimized_surface = NULL;
+    SDL_Surface *loaded_surface = IMG_Load(path);
+    
+    if (loaded_surface == NULL)
     {
         printf( "Unable to load image %s! SDL_image Error: %s\n", path, IMG_GetError() );
     }
     else
     {
-        optimizedSurface = SDL_ConvertSurface(loadedSurface, surface->format, NULL);
-        if( optimizedSurface == NULL )
+        optimized_surface = SDL_ConvertSurface(loaded_surface, surface->format, NULL);
+        if( optimized_surface == NULL )
         {
             printf( "Unable to optimize image %s! SDL Error: %s\n", path, SDL_GetError());
         }
         
-        SDL_FreeSurface(loadedSurface);
+        SDL_FreeSurface(loaded_surface);
     }
 
-    return optimizedSurface;
+    return optimized_surface;
 }
 
 int main(void)
@@ -42,13 +82,11 @@ int main(void)
     SDL_Window *window = SDL_CreateWindow("Parallax",
                                           SDL_WINDOWPOS_UNDEFINED,
                                           SDL_WINDOWPOS_UNDEFINED,
-                                          640,
-                                          480,
+                                          SCREEN_WIDTH,
+                                          SCREEN_HEIGHT,
                                           SDL_WINDOW_RESIZABLE);
+    SDL_Surface *windowSurface;
     
-
-    SDL_Surface *windowSurface = NULL;
-    int imgFlags = IMG_INIT_PNG;
     if((IMG_Init(IMG_INIT_PNG) & IMG_INIT_PNG))
     {
         windowSurface = SDL_GetWindowSurface(window);
@@ -59,70 +97,29 @@ int main(void)
         printf( "SDL_image could not initialize! SDL_image Error: %s\n", IMG_GetError() );
     }
     
-    SDL_Surface *background_1 =Parallax_LoadImage("parallax_1.png", windowSurface);
-    SDL_Surface *background_2 = Parallax_LoadImage("parallax_2.png", windowSurface);
-    SDL_Surface *background_3 = Parallax_LoadImage("parallax_3.png", windowSurface);
+    SDL_Surface *background_1 = load_image("parallax_1.png", windowSurface);
+    SDL_Surface *background_2 = load_image("parallax_2.png", windowSurface);
+    SDL_Surface *background_3 = load_image("parallax_3.png", windowSurface);
 
-    
-    SDL_Rect dst_rect_1;
-    dst_rect_1.x = 0;
-    dst_rect_1.y = 0;
-    dst_rect_1.w = 0;
-    dst_rect_1.h = 480;
-    
-    SDL_Rect dst_rect_2;
-    dst_rect_2.x = 0;
-    dst_rect_2.y = 0;
-    dst_rect_2.w = 640;
-    dst_rect_2.h = 480;
-    
-    SDL_Rect src_rect_1;
-    src_rect_1.x = 640;
-    src_rect_1.y = 0;
-    src_rect_1.w = 0;
-    src_rect_1.h = 480;
-    
-    SDL_Rect src_rect_2;
-    src_rect_2.x = 0;
-    src_rect_2.y = 0;
-    src_rect_2.w = 640;
-    src_rect_2.h = 480;
-    
-    SDL_Rect foreground_dst_rect_1;
-    foreground_dst_rect_1.x = 0;
-    foreground_dst_rect_1.y = 0;
-    foreground_dst_rect_1.w = 0;
-    foreground_dst_rect_1.h = 480;
-    
-    SDL_Rect foreground_dst_rect_2;
-    foreground_dst_rect_2.x = 0;
-    foreground_dst_rect_2.y = 0;
-    foreground_dst_rect_2.w = 640;
-    foreground_dst_rect_2.h = 480;
-    
-    SDL_Rect foreground_src_rect_1;
-    foreground_src_rect_1.x = 640;
-    foreground_src_rect_1.y = 0;
-    foreground_src_rect_1.w = 0;
-    foreground_src_rect_1.h = 480;
-    
-    SDL_Rect foreground_src_rect_2;
-    foreground_src_rect_2.x = 0;
-    foreground_src_rect_2.y = 0;
-    foreground_src_rect_2.w = 640;
-    foreground_src_rect_2.h = 480;
+    Parallax background = {};
+    init_parallax(&background, SCREEN_WIDTH, SCREEN_HEIGHT);
+    Parallax foreground = {};
+    init_parallax(&foreground, SCREEN_WIDTH, SCREEN_HEIGHT);
     
     running = true;
     
-    Sint32 focal = 0;
+    int foreground_pos = 0;
+    int background_pos = 0;
     SDL_bool down = SDL_FALSE;
     
     while (running)
     {
     
         SDL_Event event;
-        while (SDL_PollEvent(&event)) {
-            switch (event.type) {
+        while (SDL_PollEvent(&event))
+        {
+            switch (event.type)
+            {
                 case SDL_QUIT:
                 {
                     running = false;
@@ -140,48 +137,35 @@ int main(void)
         
         if (down)
         {
-            focal += 1;
+            foreground_pos += 1;
+            foreground_pos %=  SCREEN_WIDTH;
+            
+            background_pos += 2;
+            background_pos %= SCREEN_WIDTH;
+            
+            printf("foreground: %d background: %d\n", foreground_pos, background_pos);
+            
+            clip_horizontal_parallax(background_pos, &background);
+            clip_horizontal_parallax(foreground_pos, &foreground);
         }
-        
-        focal = focal % 640;
-        
-        //TODO: fix jumping
-        Uint32 background_speed = (focal / 2 ) % 640;
-        
-        dst_rect_1.w = background_speed;
-        
-        dst_rect_2.x = background_speed;
-        dst_rect_2.w = 640 - background_speed;
-
-        src_rect_1.x = 640 - background_speed;
-        src_rect_1.w = background_speed;
-        
-        src_rect_2.w = 640 - background_speed;
-        
-        foreground_dst_rect_1.w = focal;
-
-        foreground_dst_rect_2.x = focal;
-        foreground_dst_rect_2.w = 640 - focal;
-        
-        foreground_src_rect_1.x = 640 - focal;
-        foreground_src_rect_1.w = focal;
-        
-        foreground_src_rect_2.w = 640 - focal;
         
         SDL_BlitSurface(background_1, NULL, windowSurface, NULL);
         
-        SDL_BlitSurface(background_2, &src_rect_1, windowSurface, &dst_rect_1);
-        SDL_BlitSurface(background_2, &src_rect_2, windowSurface, &dst_rect_2);
+        SDL_BlitSurface(background_2, &background.src_rect_1, windowSurface, &background.dst_rect_1);
+        SDL_BlitSurface(background_2, &background.src_rect_2, windowSurface, &background.dst_rect_2);
         
-        SDL_BlitSurface(background_3, &foreground_src_rect_1, windowSurface, &foreground_dst_rect_1);
-        SDL_BlitSurface(background_3, &foreground_src_rect_2, windowSurface, &foreground_dst_rect_2);
+        SDL_BlitSurface(background_3, &foreground.src_rect_1, windowSurface, &foreground.dst_rect_1);
+        SDL_BlitSurface(background_3, &foreground.src_rect_2, windowSurface, &foreground.dst_rect_2);
 
         SDL_UpdateWindowSurface(window);
-        
-        SDL_Delay(5);
     }
+    
+    SDL_FreeSurface(background_1);
+    SDL_FreeSurface(background_2);
+    SDL_FreeSurface(background_3);
     
     IMG_Quit();
     SDL_Quit();
+    
     return 0;
 }
