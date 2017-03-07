8b4eee50ea66129eeb4f48df18901c377108a238
Tamas Losonczi <losonczitamas@Losonczis-MacBook-Air.local>
2017-03-07 13:38:43 +0100

    add baseproject

---
+++ baseproject/baseproject/main.cpp
@@ -0,0 +1,45 @@
+#include <SDL2/SDL.h>
+
+#define SCREEN_WIDTH 640
+#define SCREEN_HEIGHT 480
+
+int main(void)
+{
+    SDL_Init(SDL_INIT_VIDEO);
+    
+    SDL_Window *window = SDL_CreateWindow("Main",
+                                          SDL_WINDOWPOS_UNDEFINED,
+                                          SDL_WINDOWPOS_UNDEFINED,
+                                          SCREEN_WIDTH,
+                                          SCREEN_HEIGHT,
+                                          SDL_WINDOW_RESIZABLE);
+    
+    SDL_Renderer *renderer = SDL_CreateRenderer(window,
+                                                -1,
+                                                0);
+    
+    SDL_bool running = SDL_TRUE;
+    
+    SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
+    SDL_RenderClear(renderer);
+    SDL_RenderPresent(renderer);
+    
+    while (running)
+    {
+        SDL_Event event;
+        while (SDL_PollEvent(&event))
+        {
+            switch (event.type)
+            {
+                case SDL_QUIT:
+                {
+                    running = SDL_FALSE;
+                } break;
+            }
+        }
+    }
+    
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);    
+    SDL_Quit();
+}
