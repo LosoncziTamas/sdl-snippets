a878a0702ae61b4fa14847fc33e71d657136692b
Tamás Losonczi <losonczi.tamas91@gmail.com>
2017-06-03 11:47:36 +0200

    display debug info

--- platform/platform/platform.cpp
+++ platform/platform/platform.cpp
@@ -1,4 +1,5 @@
 #include <SDL2/SDL.h>
+#include <SDL2_ttf/SDL_ttf.h>
 
 #include "platform.h"
 
@@ -24,6 +25,8 @@ struct Game_Controller_Input
             SDL_bool right;
         };
     };
+    int32_t mouse_x;
+    int32_t mouse_y;
 };
 
 static SDL_bool handle_event(SDL_Event *event)
@@ -94,14 +97,43 @@ struct Game_State
     SDL_bool inited;
 };
 
+
+TTF_Font *font;
+struct Text_Prop
+{
+    const char *text;
+    TTF_Font *font;
+    int x;
+    int y;
+};
+
+SDL_Texture *create_font_texture(SDL_Renderer *renderer, Text_Prop *properties)
+{
+    SDL_Color white = {255, 255, 255, 0};
+    Uint32 wraplength = 2048;
+    SDL_Surface *text_surface = TTF_RenderUTF8_Blended_Wrapped(properties->font, properties->text, white, wraplength);
+    if (!text_surface)
+    {
+        printf("TTF_RenderUTF8_Solid: %s\n", TTF_GetError());
+        exit(2);
+    }
+    SDL_Texture *text_texture = SDL_CreateTextureFromSurface(renderer, text_surface);
+    SDL_FreeSurface(text_surface);
+    
+    return text_texture;
+}
+
+void render_font_texture(SDL_Renderer *renderer, SDL_Texture *text_texture, Text_Prop *properties)
+{
+    int w, h;
+    TTF_SizeUTF8(properties->font, properties->text, &w, &h);
+    SDL_Rect text_dst = {properties->x, properties->y, w, h};
+    SDL_RenderCopy(renderer, text_texture, NULL, &text_dst);
+}
+
 void flip_rect(SDL_Rect *rect)
 {
     rect->y = SCREEN_HEIGHT - rect->y - rect->h;
-}
-
-void debug_player_pos(Player *player)
-{
-    printf("tile_x %d tile_y %d x %f y %f \n", player->tile_index_x, player->tile_index_y, player->tile_rel_x, player->tile_rel_y);
 }
 
 int get_tile_value(int column, int row, World *world)
@@ -111,6 +143,19 @@ int get_tile_value(int column, int row, World *world)
         return -1;
     }
     return world->tiles[row * world->count_x + column];
+}
+
+#define DEBUG_BUFFER_SIZE 100
+
+void display_debug_info(Game_State *game_state, SDL_Renderer *renderer, Game_Controller_Input *controller)
+{
+    char buffer[DEBUG_BUFFER_SIZE];
+    Player *player = &game_state->player;
+    snprintf(buffer, DEBUG_BUFFER_SIZE, "column: %d row: %d \nx: %.2f y: %.2f\ncursor x: %d y: %d", player->tile_index_x, player->tile_index_y, player->tile_rel_x, player->tile_rel_y, controller->mouse_x , controller->mouse_y);
+    Text_Prop prop = {buffer, font, 600, 128};
+    SDL_Texture *font_texture = create_font_texture(renderer, &prop);
+    SDL_SetRenderDrawColor(renderer, 255, 255, 255, 0);
+    render_font_texture(renderer, font_texture, &prop);
 }
 
 void game_update(float delta, Game_Controller_Input *input, SDL_Renderer *renderer, Game_State *game_state)
@@ -171,8 +216,6 @@ void game_update(float delta, Game_Controller_Input *input, SDL_Renderer *render
         test.tile_rel_x = fmod(fabs(test.tile_rel_x), tile_width_in_pixel);
     }
     
-    debug_player_pos(player);
-    
     *player = test;
     
     SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
@@ -232,6 +275,9 @@ void game_update(float delta, Game_Controller_Input *input, SDL_Renderer *render
     flip_rect(&player_rect);
     SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
     SDL_RenderFillRect(renderer, &player_rect);
+    display_debug_info(game_state, renderer, input);
+
+    
     SDL_RenderPresent(renderer);
 }
 
@@ -240,6 +286,20 @@ int main(void)
 {
     SDL_Init(SDL_INIT_VIDEO);
 
+    if (TTF_Init() == -1)
+    {
+        printf("TTF_Init: %s\n", TTF_GetError());
+        exit(2);
+    }
+    
+    font = TTF_OpenFont("Pixeled.ttf", 18);
+    
+    if (!font)
+    {
+        printf("TTF_OpenFont: %s\n", TTF_GetError());
+        exit(2);
+    }
+    
     SDL_Window *window = SDL_CreateWindow("Platform",
                                           SDL_WINDOWPOS_CENTERED_MASK,
                                           SDL_WINDOWPOS_CENTERED_MASK,
@@ -275,6 +335,8 @@ int main(void)
         new_input.up = SDL_eval(state[SDL_SCANCODE_UP]);
         new_input.right = SDL_eval(state[SDL_SCANCODE_RIGHT]);
         
+        SDL_GetMouseState(&new_input.mouse_x, &new_input.mouse_y);
+
         //TODO: measure SDL_delay
         while (seconds_elapsed(start_counter, SDL_GetPerformanceCounter()) < target_seconds_per_frame);
         Uint64 end_counter = SDL_GetPerformanceCounter();
