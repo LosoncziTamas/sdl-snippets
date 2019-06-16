8f1dc9563a505dacd8cb207b7befa23a4ae3ca19
Tamás Losonczi <losonczi.tamas91@gmail.com>
2019-06-16 14:43:22 +0200

    playing with renderer

--- renderer/renderer.c
+++ renderer/renderer.c
@@ -1,6 +1,25 @@
 #include <SDL2/SDL.h>
 #include <stdbool.h>
 
+char* ReadTextFile(const char* fileName) 
+{
+    SDL_RWops *io = SDL_RWFromFile(fileName, "rb");
+    if (io) 
+    {
+        Sint64 size = SDL_RWsize(io);
+        if (size > -1) 
+        {
+            char *strMem = SDL_malloc(size + 1);
+            SDL_RWread(io, strMem, size, 1);
+            // Null terminate allocated memory
+            strMem[size] = '\0';
+            SDL_RWclose(io);
+            return strMem;
+        }
+    }
+    return NULL;
+}
+
 int main(void)
 {
     SDL_Init(SDL_INIT_VIDEO);
@@ -10,29 +29,35 @@ int main(void)
     if (SDL_CreateWindowAndRenderer(640, 480, SDL_WINDOW_ALWAYS_ON_TOP, &window, &renderer) == 0)
     {
         bool running = true;
-        SDL_Rect rect = {.x = 10, .y = 10, .w = 20, .h = 20};
+        SDL_Rect screenRect = {.x = 0, .y = 0, .w = 640, .h = 480};
 
-        SDL_SetRenderDrawColor(renderer, 0, 0, 0, SDL_ALPHA_OPAQUE);
+        SDL_SetRenderDrawColor(renderer, 64, 0, 128, SDL_ALPHA_OPAQUE);
         SDL_RenderClear(renderer);
+
+        SDL_Texture* screen = SDL_CreateTexture(renderer, SDL_PIXELFORMAT_ABGR8888, SDL_TEXTUREACCESS_TARGET, 640, 480);
+        /*SDL_SetRenderTarget(renderer, screen);
+
+        unsigned char pixels[640 * 480 * 4] = {12};
+
+        SDL_UpdateTexture(screen, &screenRect, pixels, 4 * 640);
+        SDL_RenderCopy(renderer, screen, NULL, NULL);*/
+        
+        SDL_RenderPresent(renderer);
         while(running)
         {
             for(SDL_Event event; SDL_PollEvent(&event);)
             {
                 switch (event.type)
                 {
-                    case SDL_KEYUP:
-                    {
-                        SDL_SetRenderDrawColor(renderer, 255, 255, 255, SDL_ALPHA_OPAQUE);
-                        SDL_RenderDrawRect(renderer, &rect);
-                    } break;
                     case SDL_QUIT:
                     {
                         running = false;
                     } break;
                 }
             }
-            SDL_RenderPresent(renderer);
         }
+
+        SDL_DestroyTexture(screen);
     }
 
     SDL_DestroyRenderer(renderer);
